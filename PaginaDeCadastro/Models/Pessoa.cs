de7a401f97faaeb82f821ef748b7b14df8239555
Mateus Alberto Dorna <madof007@gmail.com>
2019-01-24 19:04:29 -0200

    Adicionadas as restricões nos campos de preenchimento do cadastro

--- PaginaDeCadastro/Models/Pessoa.cs
+++ PaginaDeCadastro/Models/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,45 @@ namespace PaginaDeCadastro.Models
     public class Pessoa
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} é necessário")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho do {0} deve estar entre {2} e {1} caracteres")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "{0} é necessário")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho do {0} deve estar entre {2} e {1} caracteres")]
         public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "{0} é necessária")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "{0} é necessária")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho da {0} deve estar entre {2} e {1} caracteres")]
         public string Naturalidade { get; set; }
+
+        [Required(ErrorMessage = "{0} é necessária")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho da {0} deve estar entre {2} e {1} caracteres")]
+        [Display(Name = "Nacionalidade(Cidade)")]
         public string CidadeNac { get; set; }
+
+        [Required(ErrorMessage = "{0} é necessário")]
+        [StringLength(3, MinimumLength = 1, ErrorMessage = "Tamanho da {0} deve estar entre {2} e {1} caracteres")]
+        [Display(Name = "Nacionalidade(Estado)")]
         public string EstadoNac { get; set; }
+
+        [Required(ErrorMessage = "{0} é necessário")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho do {0} deve estar entre {2} e {1} caracteres")]
+        [Display(Name = "Nome do Pai")]
         public string NomePai { get; set; }
+
+        [Required(ErrorMessage = "{0} é necessário")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho do {0} deve estar entre {2} e {1} caracteres")]
+        [Display(Name = "Nome da Mãe")]
         public string NomeMae { get; set; }
+
         public int Idade { get; set; }
     }
 }
