ad1fcf9610f9aca653d161c69a6330579a2acda6
Mateus Alberto Dorna <madof007@gmail.com>
2019-01-24 17:34:57 -0200

    Adicionado tema bootstrap

---
+++ PaginaDeCadastro/Migrations/20190124191756_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaginaDeCadastro.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Sobrenome = table.Column<string>(nullable: true),
+                    DataNascimento = table.Column<DateTime>(nullable: false),
+                    Naturalidade = table.Column<string>(nullable: true),
+                    CidadeNac = table.Column<string>(nullable: true),
+                    EstadoNac = table.Column<string>(nullable: true),
+                    NomePai = table.Column<string>(nullable: true),
+                    NomeMae = table.Column<string>(nullable: true),
+                    Idade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+        }
+    }
+}
